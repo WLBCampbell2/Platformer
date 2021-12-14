a4278279534cc8881534736af1a80656fe4e9037
WLBCampbell2 <WLBCampbell2@glow.sch.uk>
2021-12-14 15:07:28 +0000

    Additional tweaking, still garbage

--- Game/Platformer_Game_GML.c
+++ Game/Platformer_Game_GML.c
@@ -46,44 +46,34 @@ if keyboard_check(ord("S")) && room_next(room) = -1{
   y=y+4
 }
 
-// Movement is probally not a problem
-
-
-/*
-slowly goes through platform
-// Collision with obj_plat
-if !place_meeting(x, y + 1, obj_plat){
-    move_contact_solid(270, -1)
+// Collision with objects below
+if !place_empty(x, y-1){
+  y-=1
+  execute_file("gravity_turn_off.c")
 }
-// maybe a problem*/
+// better than falling through but not great
 
 // no key
 if keyboard_check(vk_nokey){
   execute_file("no_key.c")
 }
-// Probally not a problem
 
 // alt key
 if keyboard_check(vk_alt){
   execute_file("alt_key.c")
 }
-// Probally not a problem
-/*
-//lives just dissapear
+
 // outside room
-if(obj_player.x >= 0 && obj_player.x <= room_width && obj_player.y >= 0 && obj_player.y <= room_height){
+if !(x >= 0 && x <= room_width && y >= 0 && y <= room_height){
   execute_file("outside_room.c")
 }
-// maybe a problem*/
 
 // no lives
 if room_next(room) != -1 && lives < 1{
   execute_file("no_lives.c")
 }
-// Probally not a problem
 
 // F11
 if keyboard_check(vk_f11){
   execute_file("f11_key.c")
 }
-// Probally not a problem
--- Game/outside_room.c
+++ Game/outside_room.c
@@ -1,7 +1,9 @@
 //outside room
 if room_next(room) != -1{
   lives -= 1
-  obj_player.x = xstart
-  obj_player.y = ystart
+  if lives > 0 {
+    obj_player.x = xstart
+    obj_player.y = ystart
+  }
 }
 else move_wrap(true, true, 10)
